ce0c0f34662aa426ef5fdbd6eb4b9ef6804c233a
unknown <bj2336@email.vccs.edu>
2018-10-14 07:54:09 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MovieRow from "./MovieRow.js";
+import $ from 'jquery';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.performSearch("avengers");
+  }
+
+  performSearch(searchTerm) {
+    console.log("Perform search using movieDB");
+    {/*const urlString = Enter api info here + searchTerm*/}
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=8393f0b94899d9c8a48693eb4405d52c&query=" + searchTerm;
+
+    $.ajax({
+      url: urlString,
+
+      success: (searchResults) => {
+        console.log("Fetched Data Successfully");
+        const results = searchResults.results;
+        console.log(results[0]);
+
+        const movieRows = [];
+
+        results.forEach((movie) => {
+          {/*movie.posterSource = watch video for source*/}
+
+          console.log(movie.title);
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path;
+          console.log(movie.poster_src);
+          const movieRow = <MovieRow key={movie.id} movie={movie}/>
+          movieRows.push(movieRow);
+        })
+
+        this.setState({rows: movieRows})
+      },
+
+      error: (xhr, status, err) => {
+        console.error("Failed to fetch data");
+      }
+    });
+  }
+
+  searchChangeHandler(event) {
+    console.log(event.target.value);
+    const searchTerm = event.target.value;
+    this.performSearch(searchTerm);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <table className='titleBar'>
+          <tbody>
+            <tr>
+              <td>
+                <img src="favicon.ico" alt="react logo"/>
+              </td>
+              <td>
+                <h1>MoviesDB Search</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input onChange={this.searchChangeHandler.bind(this)} placeholder="Enter search term..."/>
+
+        {this.state.rows}
       </div>
     );
   }
