a2f84edbca6d08b394f0ce5c103ae65e41ac5f98
Trip1701 <bj2336@email.vccs.edu>
2018-10-17 18:07:28 -0400

    Added Color Theme, Modified Synopsis Boxes, Softened Poster Edges, Incorporated Box-shadows

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -13,9 +13,13 @@ import './css/index.css'
       return <section className="synopsis" key={this.props.movie.id}>
               <img className="synopsis__poster" src={this.props.movie.poster_src} height="221px" width="185" alt="movie poster"/>
               <div className="synopsis__overview">
-                <h3 className="synopsis__title">{this.props.movie.title}</h3>
-                <p className="synopsis__overview">{this.props.movie.overview}</p>
-                <input className="btn__view" type="button" onClick={this.viewMovie.bind(this)} value="View" />
+                <h3 className="synopsis__overview__title">{this.props.movie.title}</h3>
+                <p className="synopsis__overview__summary">{this.props.movie.overview}</p>
+                <div className="synopsis__overview__buttons">
+                  <input className="btn__view" type="button" onClick={this.viewMovie.bind(this)} value="View" />
+                  <input className="btn__view" type="button" onClick={this.viewMovie.bind(this)} value="Site" />
+                  <input className="btn__view" type="button" onClick={this.viewMovie.bind(this)} value="View" />
+                </div>
               </div>
             </section>
     }
